2479b62dc86d085ce0fd5470f9dd25be2f84987f
skrahaman <skrahamansk@gmail.com>
2024-08-07 14:41:42 +0530

    Revert "handle httpserver"
    This reverts commit c6c9d11a185e74f6767cc8d1883fab88ee16b346.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
@@ -32,8 +31,5 @@ func main() {
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
 
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.ListenAndServe("0.0.0.0:8080", nil)
 }
